4037a2b2b5144631d92b40a4b581c3434ae24a91
njx100 <dnnguyen100@gmail.com>
2023-10-08 09:51:34 +0700

    added loading effect to fix undefined props

--- src/pages/WeatherPage/index.js
+++ src/pages/WeatherPage/index.js
@@ -3,18 +3,21 @@ import Header from "../../components/Header";
 import CitiesList from "./CitiesList";
 import { DEFAULT_USER } from "../../utilities/user";
 import axios from "axios";
+
 // import { v4 as uuidv4 } from "uuid";
 import "./style.css";
 
 const WeatherPage = () => {
   const [userData, setUserData] = useState(DEFAULT_USER);
-
+  const [isLoading, setIsLoading] = useState(true);
   const userInfoApi = "https://650c557c47af3fd22f677e50.mockapi.io/user_info/";
   const { userName, favCities, id } = userData;
 
   const getUserData = async (id) => {
+    setIsLoading(true);
     const response = await axios.get(userInfoApi + id);
     setUserData(response.data);
+    setIsLoading(false);
   };
 
   useEffect(() => {
@@ -25,7 +28,7 @@ const WeatherPage = () => {
     <div className="weather-page">
       <Header userName={userName} />
       <div className="container">
-        <CitiesList favCities={favCities} />
+        {!isLoading ? <CitiesList favCities={favCities} /> : null}
       </div>
     </div>
   );
