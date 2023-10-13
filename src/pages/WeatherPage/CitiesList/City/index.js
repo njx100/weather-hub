f7b7dee6377ff5e03bfb0f02a928ee227875353f
njx100 <dnnguyen100@gmail.com>
2023-10-13 13:45:21 +0700

    add weather condition feature

--- src/pages/WeatherPage/CitiesList/City/index.js
+++ src/pages/WeatherPage/CitiesList/City/index.js
@@ -10,17 +10,28 @@ const City = ({ name, latitude, longitude }) => {
   });
   const [isLoading, setIsLoading] = useState(true);
   const { current, forecast } = weather;
-  const [weatherStatus, setWeatherStatus] = useState("sunny");
+  const [weatherStatus, setWeatherStatus] = useState("default");
   const weatherAPI = "https://api.weatherapi.com/v1/forecast.json?key=";
   const KEY = "60b46f95aa494b69bbc15546230710";
   const apiEndPoint = "&days=6&aqi=no&alerts=no";
-  const cardClassName = `city-card city-card-${weatherStatus}`;
+  const cardClassName = `city-card ${weatherStatus}`;
+
+  const handleWeatherCondition = (text) => {
+    text = text.toLowerCase();
+    text.includes("sunny") && setWeatherStatus("sunny");
+    text.includes("clear") && setWeatherStatus("night");
+    text.includes("cloudy") && setWeatherStatus("partly-cloudy");
+    text.includes("thunder") && setWeatherStatus("thunder-storm");
+    text.includes("snow") && setWeatherStatus("snow");
+    text.includes("rain") && setWeatherStatus("rain");
+  };
 
   const getWeather = async () => {
     setIsLoading(true);
     const weatherURL = `${weatherAPI}${KEY}&q=${latitude},${longitude}${apiEndPoint}`;
     const res = await axios.get(weatherURL);
     setWeather(res.data);
+    handleWeatherCondition(res.data.current.condition.text);
     setIsLoading(false);
   };
 
@@ -34,7 +45,7 @@ const City = ({ name, latitude, longitude }) => {
         <>
           <div className="city-info">
             <div className="city-name">{name}</div>
-            <div className="city-temp">{current.temp_c}&deg;</div>
+            <div className="city-temp">{Math.round(current.temp_c)}&deg;</div>
           </div>
           <WeatherForecast name={name} forecast={forecast} />
         </>
