7862aab36d2a323310a1adc92a13f6093827b984
njx100 <dnnguyen100@gmail.com>
2023-10-13 13:13:35 +0700

    rendering weather status

--- src/pages/WeatherPage/CitiesList/City/index.js
+++ src/pages/WeatherPage/CitiesList/City/index.js
@@ -10,11 +10,11 @@ const City = ({ name, latitude, longitude }) => {
   });
   const [isLoading, setIsLoading] = useState(true);
   const { current, forecast } = weather;
-
+  const [weatherStatus, setWeatherStatus] = useState("sunny");
   const weatherAPI = "https://api.weatherapi.com/v1/forecast.json?key=";
   const KEY = "60b46f95aa494b69bbc15546230710";
   const apiEndPoint = "&days=6&aqi=no&alerts=no";
-  const cardClassName = `city-card city-card-rain`;
+  const cardClassName = `city-card city-card-${weatherStatus}`;
 
   const getWeather = async () => {
     setIsLoading(true);
