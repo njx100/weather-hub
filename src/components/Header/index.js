2bd3d8322cfc08d876282006649f4ca790015833
njx100 <dnnguyen100@gmail.com>
2023-10-14 10:50:55 +0700

    header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,17 +1,19 @@
 import { Link } from "react-router-dom";
+import SettingDropDown from "./SettingDropdown";
 import "./style.css";
 
 const Header = ({ username }) => {
   return (
     <div className="header">
-      <div className="logo"></div>
+      {/* <div className="logo"></div>
       <div className="nav-link">
         <Link to="/">Weather</Link>
         <Link to="/news">News</Link>
-      </div>
-      <div className="login">
+      </div> */}
+      {/* <div className="login">
         <Link to="/login">{username}</Link>
-      </div>
+      </div> */}
+      {/* <SettingDropDown /> */}
     </div>
   );
 };
