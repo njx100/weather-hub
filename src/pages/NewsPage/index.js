cc49344227e6e875aab65a70fda688759330b932
njx100 <dnnguyen100@gmail.com>
2023-10-18 14:47:38 +0700

    sync background with the other pages

--- src/pages/NewsPage/index.js
+++ src/pages/NewsPage/index.js
@@ -1,5 +1,5 @@
 import Header from "../../components/Header";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import axios from "axios";
 import "./style.css";
 import StoryForYou from "./StoryForYou/StoryForYouLeft";
@@ -7,16 +7,17 @@ import * as Icon from "react-feather";
 import StoryForYouRight from "./StoryForYou/StoryForYouRight";
 import MainTop from "./MainTopStart/MainTop";
 import { Circles } from "@agney/react-loading";
-
+import BackgroundContext from "../../components/BackgroundContext";
 
 const NewsPage = () => {
   const key = "gtIzOCHkHAa2NhIBjC7Zm0tMZz6AFHAy";
   const urlCard = `https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${key}`;
   const [isLoading, setIsLoading] = useState(false);
-
+  const backgroundCtx = useContext(BackgroundContext);
   const [dataNews, setdataNews] = useState([]);
-
+  const { background } = backgroundCtx;
   const [visibleItems, setVisibleItems] = useState(4);
+
   useEffect(() => {
     const fetchDataNews = async () => {
       setIsLoading(true);
@@ -30,60 +31,63 @@ const NewsPage = () => {
   const handleLoadMore = () => {
     setVisibleItems((pre) => pre + 4);
   };
+
   return (
-    <div className="bgr-color ">
-    <Header />
-   {!isLoading ? (
-    <div>
-    {dataNews.slice(11, 12).map((data) => (
-      <MainTop data={data} />
-    ))}
+    <div className={background}>
+      <Header />
+      {!isLoading ? (
+        <div>
+          {dataNews.slice(11, 12).map((data) => (
+            <MainTop data={data} />
+          ))}
 
-    {/* =======Main content ===== */}
-    <section>
-      <div className="container text-center">
-        <div className="row">
-          {/* StoryForYouLeft */}
-          <div className="col-lg-9 margin-top-4em ">
-            <div class="mb-4">
-              <h2 class="m-0 center-h2 title-story-left">
-                <Icon.Book color="white" />
-                Today's top highlights
-              </h2>
-              <p className="title-story-left p-describe">
-                Latest breaking news, pictures, videos, and special reports
-              </p>
-            </div>
+          {/* =======Main content ===== */}
+          <section>
+            <div className="container text-center">
+              <div className="row">
+                {/* StoryForYouLeft */}
+                <div className="col-lg-9 margin-top-4em ">
+                  <div class="mb-4">
+                    <h2 class="m-0 center-h2 title-story-left">
+                      <Icon.Book color="white" />
+                      Today's top highlights
+                    </h2>
+                    <p className="title-story-left p-describe">
+                      Latest breaking news, pictures, videos, and special
+                      reports
+                    </p>
+                  </div>
 
-            <div class="row gy-4">
-              {dataNews
-                .filter((data) => data.title !== "")
-                .slice(0, visibleItems)
-                .map((data) => (
-                  <StoryForYou data={data} />
-                ))}
+                  <div class="row gy-4">
+                    {dataNews
+                      .filter((data) => data.title !== "")
+                      .slice(0, visibleItems)
+                      .map((data) => (
+                        <StoryForYou data={data} />
+                      ))}
+                  </div>
+                  <div className="col-12 mt-5 text-center">
+                    <button
+                      onClick={handleLoadMore}
+                      type="button"
+                      class="btn btn-outline-primary"
+                    >
+                      Load more post
+                      <Icon.ArrowDown className="icon-arrow" size={15} />
+                    </button>
+                  </div>
+                </div>
+                {/* StoryForYouRoght */}
+                <StoryForYouRight />
+              </div>
+              <hr />
             </div>
-            <div className="col-12 mt-5 text-center">
-              <button
-                onClick={handleLoadMore}
-                type="button"
-                class="btn btn-outline-primary"
-              >
-                Load more post
-                <Icon.ArrowDown className="icon-arrow" size={15} />
-              </button>
-            </div>
-          </div>
-          {/* StoryForYouRoght */}
-          <StoryForYouRight />
+          </section>
         </div>
-        <hr />
-      </div>
-    </section>
-</div>
-
-   ): <Circles/>}
-  </div>
+      ) : (
+        <Circles />
+      )}
+    </div>
   );
 };
 
